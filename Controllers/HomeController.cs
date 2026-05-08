6254dbd2fedffc98850a5e6cf6530708c40fc262
Misa360 <ngocanhb516@gmail.com>
2026-05-08 11:22:02 +0700

    Cập nhật tính năng cho SafeMap

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,26 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Guide()
+    {
+        return View();
+    }
+
+    public IActionResult FAQ()
+    {
+        return View();
+    }
+
+    public IActionResult Terms()
+    {
+        return View();
+    }
+
+    public IActionResult Donate()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
